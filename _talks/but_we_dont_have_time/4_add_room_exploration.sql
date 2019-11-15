8131208222718b32d2886af8ca73b3c882f5b6a3
Samuel Nitsche <samuel.nitsche@smenso.de>
2019-11-15 16:13:45 +0100

    No "live-coding" at that time - too confusing for test-audience

--- _talks/but_we_dont_have_time/4_add_room_exploration.sql
+++ _talks/but_we_dont_have_time/4_add_room_exploration.sql
@@ -147,10 +147,17 @@ create or replace package body ut_deathstar_add_rooms as
       deathstar_room_manager.add_room('Testroom 6 - Section 2', -2);
 
       -- Actual
+      open c_actual for
+      	select * from deathstar_rooms where section_id < 0 order by id desc;
 
       -- Expectation
+      open c_expect for
+
+      ;
 
       -- Compare them
+      ut.expect(c_actual)
+        .to_equal(c_expect);
     end;
 end;
 /
@@ -267,7 +274,7 @@ create or replace package body ut_deathstar_add_rooms as
       ut.expect(c_actual)
         .to_equal(c_expect)
         .exclude(ut_varchar2_list('ID'))
-        .join_by('CODE')
+        .join_by('NAME')
       ;
     end;
 end;
