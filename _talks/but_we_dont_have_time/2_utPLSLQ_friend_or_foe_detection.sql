923ea9d552f822efcb0b5459b48d079677e74836
Samuel Nitsche <samuel.nitsche@smenso.de>
2020-03-25 17:29:14 +0100

    Better formatting

--- _talks/but_we_dont_have_time/2_utPLSLQ_friend_or_foe_detection.sql
+++ _talks/but_we_dont_have_time/2_utPLSLQ_friend_or_foe_detection.sql
@@ -16,8 +16,10 @@ create or replace package body ut_deathstar_friend_or_foe as
     begin
       ut.expect(
         deathstar_security.friend_or_foe(
-          t_person_appearance('lightsaber', 'red', null, null))
-        ).to_equal('FRIEND');
+          t_person_appearance(
+            'lightsaber', 'red', null, null)
+        )
+      ).to_equal('FRIEND');
     end;
 
   procedure lightsaber_blue_means_foe
@@ -25,8 +27,10 @@ create or replace package body ut_deathstar_friend_or_foe as
     begin
       ut.expect(
         deathstar_security.friend_or_foe(
-          t_person_appearance('lightsaber', 'blue', null, null))
-        ).to_equal('FOE');
+          t_person_appearance(
+            'lightsaber', 'blue', null, null)
+        )
+      ).to_equal('FOE');
     end;
 end;
 /
@@ -66,48 +70,58 @@ end;
 create or replace package body ut_deathstar_friend_or_foe as
   procedure lightsaber_red_means_friend
   as
-    begin
-      ut.expect(
-        deathstar_security.friend_or_foe(
-          t_person_appearance('lightsaber', 'red', null, null))
-        ).to_equal('FRIEND');
-    end;
+  begin
+    ut.expect(
+      deathstar_security.friend_or_foe(
+        t_person_appearance(
+          'lightsaber', 'red', null, null)
+      )
+    ).to_equal('FRIEND');
+  end;
 
   procedure lightsaber_blue_means_foe
   as
-    begin
-      ut.expect(
-        deathstar_security.friend_or_foe(
-          t_person_appearance('lightsaber', 'blue', null, null))
-        ).to_equal('FOE');
-    end;
+  begin
+    ut.expect(
+      deathstar_security.friend_or_foe(
+        t_person_appearance(
+          'lightsaber', 'blue', null, null)
+      )
+    ).to_equal('FOE');
+  end;
 
   procedure robe_black_means_friend
   as
-    begin
-      ut.expect(
-        deathstar_security.friend_or_foe(
-          t_person_appearance(null, null, 'hooded_robe', 'black'))
-        ).to_equal('FRIEND');
-    end;
+  begin
+    ut.expect(
+      deathstar_security.friend_or_foe(
+        t_person_appearance(
+          null, null, 'hooded_robe', 'black')
+      )
+    ).to_equal('FRIEND');
+  end;
 
   procedure robe_brown_means_foe
   as
-    begin
-      ut.expect(
-        deathstar_security.friend_or_foe(
-          t_person_appearance(null, null, 'hooded_robe', 'brown'))
-        ).to_equal('FOE');
-    end;
+  begin
+    ut.expect(
+      deathstar_security.friend_or_foe(
+        t_person_appearance(
+          null, null, 'hooded_robe', 'brown')
+      )
+    ).to_equal('FOE');
+  end;
 
   procedure robe_red_means_unknown
   as
-    begin
-      ut.expect(
-        deathstar_security.friend_or_foe(
-          t_person_appearance(null, null, 'hooded_robe', 'red'))
-        ).to_equal('UNKNOWN');
-    end;
+  begin
+    ut.expect(
+      deathstar_security.friend_or_foe(
+        t_person_appearance(
+          null, null, 'hooded_robe', 'red')
+      )
+    ).to_equal('UNKNOWN');
+  end;
 end;
 /
 
